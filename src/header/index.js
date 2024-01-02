237d772c9b423aa46b2aa4c30eafdff4b6206614
danny-mak1993 <danny.mak2@gmail.com>
2024-01-01 19:42:17 -0600

    revised resume for 2024

--- src/header/index.js
+++ src/header/index.js
@@ -4,7 +4,7 @@ import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import { logotext ,socialprofils } from "../content_option";
 import Themetoggle from "../components/themetoggle";
-import resumePDF from '../assets/Revised-Resume.pdf';
+import resumePDF from '../assets/Danny-Mak-Resume-2024.pdf';
 
 const Headermain = () => {
   const [isActive, setActive] = useState("false");
